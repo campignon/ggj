51ce6027c07f43d1563141887fc975ec3a05bec0
Yohann CAILLON <dreamlike.swarm@gmail.com>
2017-01-21 01:12:51 +0100

    creation des joueurs

---
+++ js/character.js
@@ -0,0 +1,22 @@
+Player = function(game, x, y, spriteName, wave1, wave2, wave3, state) {
+
+  Phaser.Sprite.call(this, game,x,y,spriteName);
+  this.life = PLAYERLIFE;
+  this.wave1 = wave1;
+  this.wave2 = wave2;
+  this.wave3 = wave3;
+  this.state = state;
+
+}
+
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function() {
+
+  //mises à jours par joueur
+  // - mettre à jour les points de vie
+  //check les input
+  //mettre à jour les waves
+
+}
---
+++ js/const.js
@@ -0,0 +1,3 @@
+const PLAYERLIFE = 50;
+const PLAYER1X = 0;
+const PLAYER1Y = 0;
