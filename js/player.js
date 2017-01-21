1cba253b580e295ac3ecfa09ee1990a3367ffc4e
Camille Pignon <camille.dourferpignon@gmail.com>
2017-01-21 17:10:22 +0100

    Adding first wave selected at launch + change color of selected wave on wave change

--- js/player.js
+++ js/player.js
@@ -7,6 +7,7 @@ var Player = function(game, id, x, y, spriteName, healthBarX, healthBarY, menu, 
   this.currentWave = 0;
   this.canSelectWave = true;
   this.menu = menu;
+  this.getCurrentWave().setState(WAVE_SELECTED);
   var healthbar = game.add.sprite(healthBarX, healthBarY, 'healthbar');
   if(id==2) {
     healthbar.scale.x *= -1;
@@ -23,20 +24,24 @@ Player.prototype.update = function() {
 };
 
 Player.prototype.setPreviousWave = function() {
+  this.getCurrentWave().setState(WAVE_DEFAULT);
   if (this.currentWave > 0) {
     this.currentWave -= 1;
   } else {
     this.currentWave = this.menu.waves.length - 1;
   }
+  this.getCurrentWave().setState(WAVE_SELECTED);
   console.log("previous wave selected : " + this.currentWave);
 };
 
 Player.prototype.setNextWave = function() {
+  this.getCurrentWave().setState(WAVE_DEFAULT);
   if (this.currentWave < this.menu.waves.length - 1) {
     this.currentWave += 1;
   } else {
     this.currentWave = 0;
   }
+  this.getCurrentWave().setState(WAVE_SELECTED);
   console.log("next wave selected : " + this.currentWave);
 };
 
